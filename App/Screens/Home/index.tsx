ff0548d25dc3e799a6bc0d997fb4f3740a32e79b
ayushsnha <ayush_snha@rediffmail.com>
2022-02-25 21:44:33 +0530

    get sliderbox

--- App/Screens/Home/index.tsx
+++ App/Screens/Home/index.tsx
@@ -1,19 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions } from 'react-native';
 import { TMDB_API_URI, TMDB_API_KEY } from '@env';
+import { SliderBox } from 'react-native-image-slider-box';
 
 import { useAxios } from '../../utils';
 
-const List = (data:any) => {
-    const { id, title } = data;
-    return (
-        <View key={id}>
-            <Text>
-                {title}
-            </Text>
-        </View>
-    );
-};
+const dimensions = Dimensions.get('screen');
 
 const ErrorUI = () => <Text>Some Error</Text>;
 
@@ -21,21 +13,31 @@ const Home = () => {
     const [movieData, setMovieData] = useState([]);
     const [gerror, setError] = useState(null);
 
-    const { response, error } = useAxios({
+    const { response, error }:any = useAxios({
         method: 'GET',
         url: `${TMDB_API_URI}/discover/movie?api_key=${TMDB_API_KEY}`,
     });
+
     useEffect(() => {
+        // console.log(response);
         if (response) {
-            setMovieData(response);
+            setMovieData(response.map((d:any) => `https://image.tmdb.org/t/p/w500/${d.poster_path}`));
         } else {
             setError(error);
         }
-    }, []);
-
+    }, [response]);
     return (
         <View>
-            {gerror ? ErrorUI() : movieData.map(List)}
+            {gerror ? ErrorUI()
+                : (
+                    <SliderBox
+                        images={movieData}
+                        autoPlay
+                        circleLoop
+                        dotStyle={{ height: 0 }}
+                        sliderBoxHeight={dimensions.height / 1.5}
+                    />
+                )}
         </View>
     );
 };
